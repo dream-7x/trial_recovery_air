f20e2ebebc38e4eaf6a0b2cdff03603f889e8781
dream-7x <gtdream7x@gmail.com>
2026-02-07 13:39:26 +0530

    buildtime: fix

--- twrp_air.mk
+++ twrp_air.mk
@@ -5,15 +5,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from generic device
-$(call inherit-product, device/xiaomi/lake/device.mk)
+$(call inherit-product, device/xiaomi/air/device.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_DEVICE := lake
-PRODUCT_NAME := twrp_lake
+PRODUCT_DEVICE := air
+PRODUCT_NAME := twrp_air
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 14C
+PRODUCT_MODEL := Redmi 13C 5G
 PRODUCT_MANUFACTURER := xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
